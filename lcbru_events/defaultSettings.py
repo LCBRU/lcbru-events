71710be289067fa20caf7675b06e8dbeb2c4c367
Richard Bramley <rabramley@gmail.com>
2015-10-14 13:30:26 +0100

    ReCaptcha theme

--- lcbru_events/defaultSettings.py
+++ lcbru_events/defaultSettings.py
@@ -7,5 +7,6 @@ DATABASE = 'databasename'
 SQLALCHEMY_DATABASE_URI = 'connection_string'
 SQLALCHEMY_ECHO = True
 
+RECAPTCHA_DATA_ATTRS = {'theme': 'blackglass'}
 RECAPTCHA_PUBLIC_KEY = 'Public Key'
 RECAPTCHA_PRIVATE_KEY = 'Private Key'
