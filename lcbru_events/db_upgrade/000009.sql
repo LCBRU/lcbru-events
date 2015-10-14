aba1987a5e57d02039e35f81e053549eaa628dd9
Richard Bramley <rabramley@gmail.com>
2015-10-14 10:58:20 +0100

    Add all practices

---
+++ lcbru_events/db_upgrade/000009.sql
@@ -0,0 +1,398 @@
+DELETE FROM genvasc_collaborators_practice;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82014')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82001')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82002')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82008')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82009')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82010')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82011')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82012')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82013')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82015')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82016')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82017')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82018')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82019')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82020')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82021')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82024')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82025')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82026')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82028')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82029')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82030')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82031')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82033')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82036')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82037')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82038')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82039')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82042')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82044')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82045')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82046')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82047')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82048')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82052')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82053')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82055')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82056')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82058')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82059')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82063')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82064')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82065')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82066')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82067')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82070')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82071')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82073')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82077')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82078')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82079')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82080')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82086')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82088')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82092')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82094')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82096')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82097')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82098')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82099')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82102')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82103')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82107')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82108')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82112')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82114')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82116')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82119')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82120')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82121')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82122')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82124')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82610')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82611')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82614')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82620')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82623')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82624')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82627')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82631')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82634')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82639')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82642')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82643')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82644')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82650')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82651')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82656')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82659')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82660')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82667')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82669')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82671')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82678')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('C82680')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('Y00137')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('Y02469')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('Y02686')
+;
+
+INSERT INTO genvasc_collaborators_practice (practiceCode)
+VALUES ('Y03587')
+;
+
